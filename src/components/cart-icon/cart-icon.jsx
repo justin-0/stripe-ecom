ed9ac8fbfe08b5aa988f227ddb1c3cfe8e78ba35
Justin OConnell <itsjustinoconnell@gmail.com>
2023-06-30 13:02:30 +0100

    reducer added to track cart quantity

--- src/components/cart-icon/cart-icon.jsx
+++ src/components/cart-icon/cart-icon.jsx
@@ -1,11 +1,20 @@
+import { useContext } from 'react';
+import { CartContext } from '../../context/cartContext';
+
 import './cart-icon.scss';
 import { ReactComponent as ShoppingIcon } from '../../assets/shopping-bag.svg';
 
 const CartIcon = ({ showCart }) => {
+	const { cartItems } = useContext(CartContext);
+
+	const totalItems = cartItems.reduce((acc, curr) => {
+		return acc + curr.quantity;
+	}, 0);
+
 	return (
 		<div className='cart-icon-container' onClick={showCart}>
 			<ShoppingIcon className='shopping-icon' />
-			<span className='item-count'>10</span>
+			<span className='item-count'>{totalItems}</span>
 		</div>
 	);
 };
